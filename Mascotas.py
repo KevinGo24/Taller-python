54a298ff7e7309427faeae1cf8feeb8d84245729
kevin <kevin@gmail.com>
2026-03-18 13:41:59 -0500

    se hizo

---
+++ Mascotas.py
@@ -0,0 +1,13 @@
+print("1- Perro")
+print("2- Conejo")
+print("3- Gato")
+Tipo_mascota = int(input('Que tipo de mascota tienes:'))
+
+if Tipo_mascota == 1:
+    print('Dog Show')
+elif Tipo_mascota == 2:
+    print('Heno')
+elif Tipo_mascota == 3:
+    print('Shuru')
+else:
+    print("Opcion invalida")

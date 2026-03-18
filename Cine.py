fb71d391cd05ac8782f5e22b26b675333073b4cd
kevin <kevin@gmail.com>
2026-03-18 12:56:28 -0500

    se terminio el ejercicio

---
+++ Cine.py
@@ -0,0 +1,7 @@
+entrada = int(input('Digite su edad: '))
+if(0 <= entrada <= 12):
+    print('Su entrada es de:  8.000')
+elif (12 <= entrada <= 59):
+    print('Su entrada es de : 12.000')
+if (entrada <= 60):
+    print ('Su entrada es de : 9.000')
